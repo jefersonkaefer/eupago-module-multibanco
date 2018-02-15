3e606bd5d45fdec1edd14bb4fbfda6e2c4106b91
Rui Barbosa <rui.barbosa@eupago.pt>
2018-02-15 17:43:25 +0000

    Module Multibanco

---
+++ Block/Info.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by euPago.pt
+ * For untitled
+ * Developer: Rui Barbosa
+ * Date: 14/02/2018
+ */
+
+namespace Eupago\Multibanco\Block;
+
+
+use Magento\Framework\Phrase;
+use Magento\Framework\Registry;
+
+
+class Info extends \Magento\Payment\Block\Info
+{
+
+    public function getSpecificInformation()
+    {
+        $informations['Entidade'] = $this->getInfo()->getAdditionalInformation('entidade');
+        $informations['Referencia'] = $this->getInfo()->getAdditionalInformation('referencia');
+        return (object)$informations;
+    }
+
+    public function getMethodCode()
+    {
+        return $this->getInfo()->getMethodInstance()->getCode();
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+## Installation
+STORES -> Configuration -> SALE/Payment Method -> euPago - Multibanco
+## Tests
+Unit tests could be found in the [Test/Unit](Test/Unit) directory.
+
+## Contributors
+Eupago Core Team
+
+## License
+[Open Source License](LICENSE.txt)
